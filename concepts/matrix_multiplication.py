d44f85f7047035ad99d2767f3a5ea1aa856ea9f6
nalawadepiyush <nalawadepiyush@gmail.com>
2019-10-05 19:11:47 +0530

    Modifying matrix multiplication

--- concepts/matrix_multiplication.py
+++ concepts/matrix_multiplication.py
@@ -1,20 +1,18 @@
 A = [[2], [1]]  # r = 2 = x , (c = 1) =  y
 B = [[2, 1]]  # (r = 1) = y , c = 2 = z
 
-x = 2
-z = 2
-y = 1
+
 
 C = []
-for i in range(0, x):
+for i in range(0, len(A)):
     l = []
-    for j in range(0, z):
+    for j in range(0, len(B[0])):
         l.append(0)
     C.append(l)
 print(C)
-for i in range(0, x):
-    for j in range(0, z):
-        C[i][j] = 0
-        for k in range(0, y):
-            C[i][j] += A[i][k] + B[k][j]
+
+for i in range(0, len(A)):
+    for j in range(0, len(B[0])):
+        for k in range(0, len(B)):
+            C[i][j] += A[i][k] * B[k][j]
 print(C)
