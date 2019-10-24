e2fce6bea60a1ab206d5c7062d89aea5c79ce9a9
nalawadepiyush <nalawadepiyush@gmail.com>
2019-10-24 18:13:58 +0530

    feat: added mergesort

--- concepts/mergesort.py
+++ concepts/mergesort.py
@@ -1,1 +1,42 @@
+def mergesort(arr, l, r):
+    if l < r:
+        m = (l + r) // 2
+        # print("mid =  {m}".format(m=m))
+        mergesort(arr, l, m)
+        mergesort(arr, m + 1, r)
+        merge(arr, l, m, r)
 
+
+def merge(arr, l, m, r):
+    L = arr[l:m + 1]
+    R = arr[m + 1:r + 1]
+
+    # print(L, len(L))
+    # print(R, len(R))
+
+    i = 0
+    j = 0
+    k = l  # index of last merged array
+
+    while i < len(L) and j < len(R):
+        if L[i] <= R[j]:
+            arr[k] = L[i]
+            i += 1
+        else:
+            arr[k] = R[j]
+            j += 1
+        k += 1
+    while i < len(L):
+        arr[k] = L[i]
+        i += 1
+        k += 1
+    while j < len(R):
+        arr[k] = R[j]
+        j += 1
+        k += 1
+    # print(arr)
+
+
+a = [1, 10, 7, 4, 3, 2]
+mergesort(a, 0, 5)
+print(a)
