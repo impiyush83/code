671ad0b1ab74ee907e85d8b03ac75e95461b993e
nalawadepiyush <nalawadepiyush@gmail.com>
2019-11-21 17:35:40 +0530

    feat: added leectcode problems

---
+++ leetcode/655-Non-decreasing-array.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def checkPossibility(self, nums: List[int]) -> bool:
+        p = None
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i+1]:
+                if p is not None:
+                    return False
+                p = i
+        print(p)
+        return p is None or p == 0 or p == len(nums) - 2 or nums[p - 1] <= nums[p + 1] or nums[p] <= nums[p + 2]
