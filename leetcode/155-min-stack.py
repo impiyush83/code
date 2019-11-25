10042b63633c0d6aabbba7bb2cf98c70a380a038
nalawadepiyush <nalawadepiyush@gmail.com>
2019-11-25 22:18:08 +0530

    feat: added leetcode problems

--- leetcode/155-min-stack.py
+++ leetcode/155-min-stack.py
@@ -4,26 +4,31 @@ class MinStack:
         """
         initialize your data structure here.
         """
-        self.ll = []
+        self.stack = []
 
-    def push(self, x: int) -> None:
-        self.ll.append(x)
+    def push(self, x):
+        self.stack.append((x, min(x, self.getMin())))
 
-    def pop(self) -> None:
-        self.ll.pop()
+    def pop(self):
+        if len(self.stack) == 0:
+            return None
+        return self.stack.pop()
 
-    def top(self) -> int:
-        if self.ll:
-            return self.ll[-1]
+    def top(self):
+        if len(self.stack) == 0:
+            return None
+        return self.stack[-1][0]
 
-    def getMin(self) -> int:
-        return min(self.ll)
+    def getMin(self):
+        if len(self.stack) == 0:
+            return float('inf')
+        return self.stack[-1][1]
 
 
-# Your MinStack object will be instantiated and called as such:
-x = 5
-obj = MinStack()
-obj.push(x)
-obj.pop()
-param_3 = obj.top()
-param_4 = obj.getMin()
+# # Your MinStack object will be instantiated and called as such:
+# x = 5
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
