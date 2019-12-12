081fa38d7b5ab6c55996aff332f1091d55c12962
impiyush83 <nalawadepiyush@gmail.com>
2019-12-12 15:59:41 +0530

    feat: added leetcode problems

--- interview/Dream11/perfect_strings.py
+++ interview/Dream11/perfect_strings.py
@@ -1,24 +1,28 @@
-MAX_DIGITS = 10
+from collections import Counter
 
-
-def check(freq, k):
-    for i in range(0, MAX_DIGITS):
-        if freq[i] and freq[i] != k:
-            return False
-    return True
+def getSubstrings(s):
+    all_substrings = []
+    for i in range(0, len(s)):
+        for j in range(0, len(s) - i):
+            sub_string = s[j:j + i + 1]
+            if sub_string not in all_substrings:
+                yield sub_string
 
 
 def perfectSubstring(s, k):
-    # Write your code here
     result = 0
-    for i in range(0, len(s)):
-        freq = [0] * MAX_DIGITS
-        for j in range(i, len(s)):
-            index = ord(s[j]) - ord('0')
-            freq[index] += 1
-
-            if freq[index] > k:
+    for substring in getSubstrings(s):
+        freq_count = Counter(substring)
+        flag = 1
+        for element in freq_count:
+            if freq_count[element] != k:
+                flag = 0
                 break
-            elif freq[index] == k and check(freq, k) == True:
-                result += 1
+        if flag:
+            result += 1
     return result
+
+
+s = input()
+k = int(input())
+print(perfectSubstring(s, k))
