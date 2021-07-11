fc2ed59b86614a50e1569a529bbc9576a64b7ab0
impiyush83 <nalawadepiyush@gmail.com>
2021-07-12 04:43:05 +0530

    feat: Added database queries

--- leetcode/176-second_highest_salary.py
+++ leetcode/176-second_highest_salary.py
@@ -26,6 +26,10 @@ SELECT IFNULL((SELECT DISTINCT Salary FROM Employee ORDER BY Salary DESC
 LIMIT 1 OFFSET 1), NULL) AS SecondHighestSalary;
 
 
+SELECT max(Salary)
+FROM Employee
+WHERE Salary < (SELECT max(Salary) FROM Employee);
+
 """
 
 
